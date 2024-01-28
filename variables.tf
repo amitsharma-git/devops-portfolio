f72bc88836212cb4d367e2bef7a856304dc6f8d8
Amit Sharma <amitsharma.ambah@gmail.com>
2024-01-28 23:10:37 +0530

    dev:k8s cluster provisioning:

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "AWS_ACCESS_KEY_ID" {
   description = "access key id"
+  type        = string
   default     = ""
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  description = " secret access key"
+  description = "secret access key"
+  type        = string
   default     = ""
 }
 
 variable "region" {
   description = "aws default region"
+  type        = string
   default     = "us-east-1"
 }
